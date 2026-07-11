3751c817138d460db274abdb7dd0579f5241843d
Yuna Won <wonyoona31@gmail.com>
2026-07-11 17:52:08 +0900

    pjson

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/movie-search-app",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
