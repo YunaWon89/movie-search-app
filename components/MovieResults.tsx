4b316752ed253a1dd231d1bc5671b4d62f16dd78
Yuna Won <wonyoona31@gmail.com>
2026-07-14 11:01:08 +0900

    Test without guest session

--- components/MovieResults.tsx
+++ components/MovieResults.tsx
@@ -14,7 +14,7 @@ export default async function MovieResults({ query, page }: MovieResultsProps) {
   const cookieStore = await cookies();
   const guestSessionId = cookieStore.get(GUEST_SESSION_COOKIE)?.value;
 
-  const data = await searchMovies(query, page, guestSessionId);
+  const data = await searchMovies(query, page);
   const movies = data.results;
 
   if (movies.length === 0) {
